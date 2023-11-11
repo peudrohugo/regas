63feffa9998a58f44897d924f76d1f10d66a2d02
Pedro Hugo <pedrohugo.ov09@gmail.com>
2023-11-10 21:39:39 -0300

    Validação na tela de login

--- regas/src/app/page.tsx
+++ regas/src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
 import { Card, CardBody, Container } from "@chakra-ui/react";
 import SignInForm from "./components/SignInForm";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default async function Home() {
+export default function Home() {
+  const router = useRouter();
+  let containsDriver: boolean = true;
+
+  useEffect(() => {
+    const driver = localStorage.getItem("driver");
+    if (!driver) {
+      containsDriver = false;
+    }
+
+    if (containsDriver) {
+      router.push("/fuel");
+    }
+  }, []);
+
   return (
     <>
       <Container>
