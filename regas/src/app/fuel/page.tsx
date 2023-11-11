44eeedca3fec17049fc06f1f06ef923a56bd0dd8
Pedro Hugo <pedrohugo.ov09@gmail.com>
2023-11-10 21:36:15 -0300

    Remoção de código na tela de registrar abastecimento

--- regas/src/app/fuel/page.tsx
+++ regas/src/app/fuel/page.tsx
@@ -2,23 +2,6 @@ import { Card, CardBody, Container } from "@chakra-ui/react";
 import Footer from "../components/Footer";
 import FuellingForm from "../components/FuellingForm";
 
-async function create(formData: FormData) {
-  "use server";
-  const name = formData.get("name")?.valueOf();
-
-  const response = await fetch(`http://localhost:3333/driver`, {
-    method: "POST",
-    body: JSON.stringify({
-      name,
-    }),
-    headers: {
-      "content-type": "application/json",
-    },
-  });
-
-  const data = await response.json();
-}
-
 export default async function Fuel() {
   return (
     <>
