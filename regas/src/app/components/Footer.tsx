79367c81d9918004b395741b39395057c2f15252
Pedro Hugo <pedrohugo.ov09@gmail.com>
2023-11-10 21:34:15 -0300

    Alterações nos ícones do Footer

--- regas/src/app/components/Footer.tsx
+++ regas/src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import * as Unicons from "@iconscout/react-unicons";
+import { FaGasPump, FaHistory } from "react-icons/fa";
 
 export default function Footer(props: { path: string }) {
   const { path } = props;
@@ -7,20 +7,20 @@ export default function Footer(props: { path: string }) {
     <footer>
       {path == "fuelling-history" ? (
         <>
-          <Link href="/">
-            <Unicons.UilPump size="30" color="#556270" />
+          <Link href="/fuel">
+            <FaGasPump color="#556270" fontSize="1.5em" />
           </Link>
           <Link href="">
-            <Unicons.UilHistory size="30" color="#FFFFFF" />
+            <FaHistory color="white" fontSize="1.5em" />
           </Link>
         </>
       ) : (
         <>
           <Link href="">
-            <Unicons.UilPump size="30" color="#FFFFFF" />
+            <FaGasPump color="white" fontSize="1.5em" />
           </Link>
           <Link href="/fuelling-history">
-            <Unicons.UilHistory size="30" color="#556270" />
+            <FaHistory color="#556270" fontSize="1.5em" />
           </Link>
         </>
       )}
