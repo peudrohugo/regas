e3120d67cc08a59c19be9cb0cf5127210f40d457
Pedro Hugo <pedrohugo.ov09@gmail.com>
2023-11-10 21:35:53 -0300

    Tela de histórico de abastecimentos

--- regas/src/app/fuelling-history/page.tsx
+++ regas/src/app/fuelling-history/page.tsx
@@ -1,8 +1,17 @@
+import { Card, CardBody, Container } from "@chakra-ui/react";
 import Footer from "../components/Footer";
+import FuellingHistoryTable from "../components/FuellingHistoryTable";
 
 export default function FuellingHistory() {
   return (
     <>
+      <Container>
+        <Card>
+          <CardBody>
+            <FuellingHistoryTable />
+          </CardBody>
+        </Card>
+      </Container>
       <Footer path={"fuelling-history"} />
     </>
   );
