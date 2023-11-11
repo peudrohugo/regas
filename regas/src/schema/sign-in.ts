487c26d711bc4a23df2449a92ae2ebdfee6b73d2
Pedro Hugo <pedrohugo.ov09@gmail.com>
2023-11-10 21:35:36 -0300

    Zod Schema para formulário de login

--- regas/src/schema/sign-in.ts
+++ regas/src/schema/sign-in.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 export const signInSchema = z.object({
-  name: z.string().max(10, { message: "Must be 10 or fewer characters long" }),
+  name: z.string().max(20, { message: "Must be 10 or fewer characters long" }),
 });
 
 export type Driver = z.infer<typeof signInSchema>;
