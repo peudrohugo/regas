8788068014045e32e7ba006e9a8162e16d53c2cf
Pedro Hugo <pedrohugo.ov09@gmail.com>
2023-11-10 21:38:56 -0300

    Requisição para registro de abastecimento

--- regas/src/app/components/FuellingForm.tsx
+++ regas/src/app/components/FuellingForm.tsx
@@ -4,12 +4,16 @@ import {
   FormErrorMessage,
   FormLabel,
   FormControl,
-  Input,
   Button,
   Spinner,
   Select,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  useToast,
 } from "@chakra-ui/react";
-import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { fuelSchema } from "../../schema/fuel";
@@ -17,42 +21,68 @@ import { fuelSchema } from "../../schema/fuel";
 type FormData = z.infer<typeof fuelSchema>;
 
 export default function FuellingForm() {
-  const router = useRouter();
   const {
     handleSubmit,
     register,
     formState: { errors, isSubmitting, isDirty, isValid },
-  } = useForm<FormData>({
-    resolver: zodResolver(fuelSchema),
-  });
+  } = useForm<FormData>();
+  const toast = useToast();
 
   async function onSubmit(data: FormData) {
-    console.log(isSubmitting);
-    console.log(data);
-    // Replace this with a server action or fetch an API endpoint to authenticate
-    await new Promise<void>((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 2000); // 2 seconds in milliseconds
+    const driver = JSON.parse(localStorage.getItem("driver") || "");
+
+    const response = await fetch("http://localhost:3333/refuelling", {
+      method: "post",
+      mode: "cors",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        quantity: parseInt(`${data.amount}`),
+        fuelType: data.type,
+        driverId: driver?.id,
+      }),
     });
+
+    if (response.status == 201) {
+      toast({
+        title: `Registered fuelling`,
+        description: "Fuelling registred successfully!",
+        status: "success",
+        duration: 3000,
+      });
+    }
+    if (response.status == 400 || response.status == 500) {
+      toast({
+        title: `Could not sign in`,
+        description:
+          "Something went wrong when trying to register your fuelling!",
+        status: "error",
+        duration: 3000,
+      });
+    }
   }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <FormControl>
         <FormLabel htmlFor="amount">Quantity(in liters): </FormLabel>
-        <Input
-          id="amount"
-          placeholder="Enter fuel amount"
-          {...register("amount", { required: true })}
-        />
+        <NumberInput id="amount" defaultValue={1} min={1} max={100}>
+          <NumberInputField {...register("amount", { required: true })} />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
         <FormErrorMessage>
           {errors.amount && errors.amount.message}
         </FormErrorMessage>
       </FormControl>
-      <FormControl>
+      <FormControl mt={4}>
         <FormLabel htmlFor="type">Type: </FormLabel>
-        <Select id="type" placeholder="Select fuel type">
+        <Select
+          id="type"
+          placeholder="Select fuel type"
+          {...register("type", { required: true })}
+        >
           <option value="GASOLINE">Gasoline</option>
           <option value="ETANOL">Etanol</option>
           <option value="DIESEL">Diesel</option>
